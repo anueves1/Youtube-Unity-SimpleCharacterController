52ca37999156e80027b570e85c6d03f212f44665
anueves1 <34652784+ProfStudGit@users.noreply.github.com>
2018-09-17 19:10:48 +0200

    asd

--- SimpleCharacterController/Assets/Scripts/CharacterMotor.cs
+++ SimpleCharacterController/Assets/Scripts/CharacterMotor.cs
@@ -1,16 +1,39 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class CharacterMotor : MonoBehaviour {
+public class CharacterMotor : MonoBehaviour
+{
+    [SerializeField]
+    private float m_Speed = 2;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private InputHandler m_Input;
+    private CharacterController m_CharacterController;
+
+    private void Start()
+    {
+        //Get a reference to the character controller.
+        m_CharacterController = GetComponent<CharacterController>();
+
+        //Get a reference to the input handler.
+        m_Input = Manager.Instance.InputHandler;
+    }
+
+    private void FixedUpdate()
+    {
+        //Get the vector that would move the character horizontally.
+        var horizontal = m_Input.Horizontal * transform.right;
+
+        //Get the vector that would move the character vertically.
+        var vertical = m_Input.Vertical * transform.forward;
+        
+        //Get the movement vector.
+        var moveDir = horizontal + vertical;
+        //Normalize the movement vector.
+        moveDir.Normalize();
+
+        //Add the speed to the movement vector.
+        moveDir *= m_Speed;
+
+        //Move in that direction.
+        m_CharacterController.Move(moveDir);
+    }
 }
