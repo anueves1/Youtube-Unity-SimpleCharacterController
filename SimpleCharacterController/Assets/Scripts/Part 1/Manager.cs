b9e19af4138c699d17ff6284ba2dfe45f6b7cf94
anueves1 <34652784+ProfStudGit@users.noreply.github.com>
2018-09-19 19:05:35 +0200

    asd

---
+++ SimpleCharacterController/Assets/Scripts/Part 1/Manager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace Part1
+{
+    public class Manager : MonoBehaviour
+    {
+        public static Manager Instance {
+            get
+            {
+                //Try to find a manager object in the scene.
+                var manager = FindObjectOfType<Manager>();
+
+                //If there's no manager object.
+                if (manager == null)
+                {
+                    //Create a new manager object.
+                    var nObject = new GameObject("Manager");
+                    //Add the manager component.
+                    var nManager = nObject.AddComponent<Manager>();
+
+                    //Use the new object as a manager.
+                    manager = nManager;
+                }
+
+                return manager;
+            }
+        }
+
+        public InputHandler InputHandler { get; private set; }
+
+        private void Awake()
+        {
+            //Find the input handler.
+            InputHandler = FindObjectOfType<InputHandler>();
+        }
+    }
+}
