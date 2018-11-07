1d2a31df52d68f0c3d0fd4269a3689596fe5aa01
anueves1 <34652784+ProfStudGit@users.noreply.github.com>
2018-11-07 19:40:06 +0100

    asd

---
+++ SimpleCharacterController/Assets/Finished/Scripts/Controller/InputManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Finished
+{
+    public class InputManager : MonoBehaviour
+    {
+        public bool IsRunning { get; private set; }
+        public bool Jump { get; private set; }
+
+        public float Horizontal { get; private set; }
+        public float Vertical { get; private set; }
+
+        public float MouseX { get; private set; }
+        public float MouseY { get; private set; }
+
+        private void Update()
+        {
+            Horizontal = Input.GetAxisRaw("Horizontal");
+            Vertical = Input.GetAxisRaw("Vertical");
+
+            Jump = Input.GetKeyDown(KeyCode.Space);
+            IsRunning = Input.GetKey(KeyCode.LeftShift);
+
+            MouseX = Input.GetAxisRaw("Mouse X");
+            MouseY = Input.GetAxisRaw("Mouse Y");
+        }
+    }
+}
